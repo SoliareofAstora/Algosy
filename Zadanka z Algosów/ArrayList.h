9663bd5e4befc15fa318d825f1179d55c370d5c4
Piotr Kucharski <piotr1kucharski@gmail.com>
2017-10-31 22:05:53 +0100

    Arrays the beginning

---
+++ Zadanka z Algosów/ArrayList.h
@@ -0,0 +1,6 @@
+#ifndef ArrayList_h
+#define ArrayList_h
+
+
+
+#endif
---
+++ Zadanka z Algosów/main.cpp
@@ -0,0 +1,26 @@
+﻿/*
+A x - dodaj liczbę 0 ≤ x ≤ 106 do listy
+F x - wypisz "TRUE" jeżeli x jest obecne w liście, "FALSE" w przeciwnym razie
+D - usuń z listy pierwszy element i go wypisz, jeśli lista jest pusta wypisz "EMPTY"
+S - wypisz rozmiar listy
+
+
+10
+A 6210
+D
+A 9704
+A 3231
+A 530
+A 7019
+F 3022
+A 317
+D
+D
+ */
+#include <cstdlib>
+
+int main()
+{
+	system("PAUSE");
+	return 0;
+}
