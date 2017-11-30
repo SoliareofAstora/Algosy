54962ad18063d49ae40ea8cec2d7a95ab44c883d
Piotr Kucharski <piotr1kucharski@gmail.com>
2017-11-30 19:22:33 +0100

    Clean up

--- Zadanka z Algosów/QSort.cpp
+++
@@ -1,9 +0,0 @@
-#include <xutility>
-static int compare(const void * a, const void * b)
-{
-	return (*(int*)a - *(int*)b);
-}
-static void QSort(int *arr,int size)
-{
-	qsort(arr, size, sizeof (int), compare);
-}
--- Zadanka z Algosów/genReverse.cpp
+++
@@ -1,9 +0,0 @@
-static int* genReverse(int size)
-{
-	int *arr = new int[size];
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = size-i;
-	}
-	return arr;
-}
