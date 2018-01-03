4f41bb1176bf2a8829d5a30fbabc8f773ccb989b
Piotr Kucharski <piotr1kucharski@gmail.com>
2018-01-03 09:15:06 +0100

    rar of prev assigment

--- Zadanka z Algosów/FindingLeader.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-static void FindLeader()
-{
-	std::cerr << "coœtam";
-}
--- Zadanka z Algosów/genSorted.cpp
+++
@@ -1,9 +0,0 @@
-static int* genSorted(int size)
-{
-	int *arr = new int[size];
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = i;
-	}
-	return arr;
-}
