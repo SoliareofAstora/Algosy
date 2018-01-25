5ae859ef38ee7845d199fd071d40b13b7cc89464
Piotr Kucharski <piotr1kucharski@gmail.com>
2018-01-25 17:46:52 +0100

    bst ongoing

--- Zadanka z Algosów/BST.h
+++ Zadanka z Algosów/BST.h
@@ -5,21 +5,18 @@ struct node
 	int key;
 	node *left, *right;
 };
-/*
-insert(x) - wstawia element 'x' do drzewa
-search(x), searchRecursive(x) - sprawdza czy element nale¿y do drzewa, zwraca wskaŸnik do wêz³a lub nullptr.Napisaæ wersjê iteracyjn¹ i rekurencyjn¹
-size() - zwraca liczbê wêz³ów
-minimum() - zwraca wartoœæ najmniejszego elementu
-maximum() - zwraca wartoœæ najwiêkszego elementu
-depth() - zwraca wysokoœæ drzewa
-*/
+
 class BST
 {
+	int max(int a, int b)
+	{
+		return a > b ? a : b;
+	}
 public:
 	node * root;
 	int Size = 0;
 
-	BST() :root(NULL) {};
+	BST(){};
 	BST(int item)
 	{
 		root = newNode(item);
@@ -30,57 +27,102 @@ public:
 		return Size;
 	}
 
+	int depth()
+	{
+		int out = 0;
+		return depth(root,out);
+	}
+	int depth(struct node* nextNode, int out)
+	{
+		if (nextNode == nullptr)
+		{
+			return out;
+		}
+		else
+		{
+			out++;
+			int temp1 = depth(nextNode->left, out);
+			int temp2 = depth(nextNode->right, out);
+			return max(temp1, temp2);
+		}
+	}
+
 	struct node *newNode(int item)
 	{
 		Size++;
 		struct node *temp = static_cast<struct node *>(malloc(sizeof(struct node)));
 		temp->key = item;
-		temp->left = temp->right = NULL;
+		temp->left = temp->right = nullptr;
 		return temp;
 	}
 
+	int minimum()
+	{
+		return minimum(root);
+	} 
+	int maximum()
+	{
+		return maximum(root);
+	}
 
+	int minimum(struct node *nextNode)
+	{
+		if (nextNode->left==nullptr)
+		{
+			return nextNode->key;
+		}
+		return minimum(nextNode->left);
+	}
+
+	int maximum(struct node *nextNode)
+	{
+		if (nextNode->right == nullptr)
+		{
+			return nextNode->key;
+		}
+		return maximum(nextNode->right);
+	}
 
 	/* displaying stuff*/
 	void inorder()
 	{
 		inorder(root);
 	}
-	void inorder(struct node *newnode)
+	void inorder(struct node *nextNode)
 	{
-		if (newnode != NULL)
+		if (nextNode != nullptr)
 		{
-			inorder(newnode->left);
-			printf("%d ", newnode->key);
-			inorder(newnode->right);
+			inorder(nextNode->left);
+			printf("%d ", nextNode->key);
+			inorder(nextNode->right);
 		}
 	}
 	void preorder()
 	{
 		preorder(root);
 	}
-	void preorder(struct node *newnode)
+	void preorder(struct node *nextNode)
 	{
-		if (newnode != NULL)
+		if (nextNode != nullptr)
 		{
-			printf("%d ", newnode->key);
-			preorder(newnode->left);
-			preorder(newnode->right);
+			printf("%d ", nextNode->key);
+			preorder(nextNode->left);
+			preorder(nextNode->right);
 		}
 	}
 	void postorder()
 	{
 		postorder(root);
 	}
-	void postorder(struct node *newnode)
+	void postorder(struct node *nextNode)
+	{
+		if (nextNode != nullptr)
 		{
-			if (newnode != NULL)
-			{
-				postorder(newnode->right);
-				postorder(newnode->left);
-				printf("%d ", newnode->key);
-			}
+			postorder(nextNode->right);
+			postorder(nextNode->left);
+			printf("%d ", nextNode->key);
 		}
+	}
 	
 	void insert(int key)
 	{
@@ -89,7 +131,7 @@ public:
 	
 	struct node* insert(struct node* node, int key)
 	{
-		if (node == NULL) return newNode(key);
+		if (node == nullptr) return newNode(key);
 
 		if (key < node->key)
 			node->left = insert(node->left, key);
@@ -105,31 +147,24 @@ public:
 		return searchRecursive(root, key);
 	}
 
-	struct node* searchRecursive(struct node*root, int key)
+	struct node* searchRecursive(struct node*nextNode, int key)
 	{
-		if (root == NULL || root->key == key)
-			return root;
+		if (nextNode == nullptr || nextNode->key == key)
+			return nextNode;
 
-		if (root->key < key)
-			return searchRecursive(root->right, key);
+		if (nextNode->key < key)
+			return searchRecursive(nextNode->right, key);
 
-		return searchRecursive(root->left, key);
+		return searchRecursive(nextNode->left, key);
 	}
 
 	struct node* search(int key)
 	{
-		return search(root, key);
+		node* out = root;
+		do
+		{
+			
+		} while (false);
+		return nullptr;
 	}
-
-	struct node* search(struct node*root, int key)
-	{
-		if (root == NULL || root->key == key)
-			return root;
-
-		if (root->key < key)
-			return searchRecursive(root->right, key);
-
-		return searchRecursive(root->left, key);
-	}
-
 };
--- Zadanka z Algosów/main.cpp
+++ Zadanka z Algosów/main.cpp
@@ -1,20 +1,25 @@
 // C program to demonstrate insert operation in binary search tree
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <iostream>
 #include "BST.h"
 
 // Driver Program to test above functions
 int main()
 {
-	BST tree(10);
+	BST tree=BST(10);
+	tree.insert(35);
+	std::cerr << tree.depth() << std::endl;
 	tree.insert(50);
 	tree.insert(30);
 	tree.insert(20);
+	std::cerr << tree.depth() << std::endl;
 	tree.insert(40);
 	tree.insert(70);
 	tree.insert(60);
+	std::cerr << tree.depth() << std::endl;
 	tree.insert(80);
+	tree.insert(51);
+	std::cerr << tree.depth() << std::endl;
 
 	// print inoder traversal of the BST
 	tree.inorder();
@@ -25,7 +30,7 @@ int main()
 	std::cerr << std::endl;
 
 	node* tmp = nullptr;
-	tmp = tree.search(10);
+	tmp = tree.search(30);
 	if (tmp == nullptr)
 	{
 		std::cerr <<"nie ma takiej wartoœci\n";
@@ -52,7 +57,11 @@ int main()
 	{
 		std::cerr << "znaleziono\n";
 	}
-	std::cerr << tree.size<<std::endl;
+
+	std::cerr << tree.depth() << std::endl;
+	std::cerr << tree.size() << std::endl;
+	std::cerr << tree.minimum() << std::endl;
+	std::cerr << tree.maximum() << std::endl;
 
 
 	system("pause");
